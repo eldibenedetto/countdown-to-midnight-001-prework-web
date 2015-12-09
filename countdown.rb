f6aa6ec3b61c1b203d38e201a045bd397cca9eca
Eugene DiBenedetto <eldibenedetto@gmail.com>
2015-12-09 10:48:10 -0500

    countdown

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+time_left = 10
 
-def countdown
+def countdown(time_left)
+    while time_left > 0 do
+        puts "#{time_left} SECOND(S)!"
+        time_left -= 1
+        end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(time_left)
+    while time_left > 0 do
+        puts "#{time_left} SECOND(S)!"
+        time_left -= 1
+        sleep(1)
+        end
+    "HAPPY NEW YEAR!"
 end
